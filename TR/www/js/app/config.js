f0b6821acbd160f53d664e96c2a31a7b08475890
charan0514 <shree.adiga@gmail.com>
2016-04-22 15:54:13 +0530

    added data.json

--- TR/www/js/app/config.js
+++ TR/www/js/app/config.js
@@ -3,14 +3,14 @@ var app = angular.module('trApp', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/login');
     $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: 'partials/home.html',
-            controller: 'homeController'
+        .state('navigation', {
+            url: '/navigation',
+            templateUrl: 'partials/navigation.html',
+            //controller: 'navigationController'
         })
         .state('login', {
             url: '/login',
             templateUrl: 'partials/login.html',
-            controller: 'loginController'
+            //controller: 'loginController'
         })
 });

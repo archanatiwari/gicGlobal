fcafc56f03279648dd6f62685f1a6e8e3893ddea
charan0514 <shree.adiga@gmail.com>
2016-04-22 19:45:18 +0530

    login, services and map related changes

--- TR/www/js/app/controllers/loginController.js
+++ TR/www/js/app/controllers/loginController.js
@@ -1,3 +1,29 @@
-app.controller('loginController', function(){
+app.controller('loginController', function($scope, $state, SharedFactory, SharedDataService){
+
+	$scope.userData = null; 
+	
+	SharedFactory.getData().then(function(response) {
+        $scope.userData = response;
+    }, function(response) {
+        $scope.userData = response;
+    });
+	
+	//hard-coded login
+	$scope.email = "archit@globant.com", $scope.password = "1234abcd";
+
+    $scope.doLogin = function() {
+        var userData = $scope.userData;
+        for (var i = 0; i < userData.length; i++) {
+            var user = userData[i];
+            if (user.email == $scope.email && user.password == $scope.password) {
+                SharedDataService.setCurrentUser(user);
+                $state.go('home');
+                break;
+            } else {
+                console.log('Enter correct username and password');
+                // $scope.errorMsg = true;
+            }
+        }
+    }
 	
 });
